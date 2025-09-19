bd7485e8bdd0eb9dac249e8ebf9dc7d24c3e411c
Obinna Nwobodo <obinnanwobodo27@gmail.com>
2025-09-19 17:30:31 +0100

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Navbar from "@/Components/Navbar/Navbar";
 import Hero from "@/Components/Hero/Hero";
 import Services from "@/Components/Services/Services";
-import Case from "@/Components/Case/case";
+import Case from "@/Components/Case/Case";
 
 export default function Home() {
   return (
